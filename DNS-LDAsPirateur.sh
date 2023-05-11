a8805ecc23cd05438f14d9e43f7fef57f53e3726
penthium2 <penthium2@hotmail.fr>
2023-05-11 17:41:05 +0200

    Update DNS-LDAsPirateur.sh

--- DNS-LDAsPirateur.sh
+++ DNS-LDAsPirateur.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 while [[ -n "$1" ]] ; do
-        case $1 in
-	  -h|--host)
-        	ldap="$2"
-	        shift 2
-	        ;;
-	  -u|--user)
+     case $1 in
+          -h|--host)
+                ldap="$2"
+                shift 2
+                ;;
+          -u|--user)
               user="$2"
               shift 2
-	       ;;
-	  -p|--password)
+               ;;
+          -p|--password)
               password="$2"
               shift 2
-	       ;;
-          -d|--domain)
-              domain="$2"
+               ;;
+          -d|-domain)
+              domain=$2
               shift 2
-	      ;;
-	  *)
-	      echo "syntax error"
-	      exit 2
-		;;
-	esac
+              ;;
+          *)
+              echo "syntax error"
+              exit 2
+              ;;
+      esac
 done
 
 ldapsearch -LLL -H ldap://${ldap}:389 -b "CN=MicrosoftDNS,DC=DomainDnsZones,${domain}"  -w "${password}" -U "${user}" '(objectClass=dnsNode)' -E pr=10000/noprompt | grep -A 1 '^dn:' | sed -nE '/--/d;s/dn: DC=(.*)CN=MicrosoftDNS.*$/\1/;s/^([^,]+),DC=([^,]+),$/\1.\2/p'
