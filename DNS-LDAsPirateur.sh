c0bd54f5742407c27ff3c7444b25c422964bcc13
Rémi GASCOU (Podalirius) <podalirius@protonmail.com>
2023-05-11 17:49:04 +0200

    Autoformatting the domain with DC=domain,DC=com ldap syntax

--- DNS-LDAsPirateur.sh
+++ DNS-LDAsPirateur.sh
@@ -27,6 +27,9 @@ while [[ -n "$1" ]] ; do
 	      esac
 done
 
+# Format the domain
+domain="DC="$(echo "${domain}" | sed 's/\./,DC=/g')
+
 ldapsearch -Q -LLL -H "ldap://${ldap}:389" -b "CN=MicrosoftDNS,DC=DomainDnsZones,${domain}" -w "${password}" -U "${user}" '(objectClass=dnsNode)' -E pr=${results_per_page}/noprompt \
  | grep -A 1 '^dn:' \
  | sed -nE '/--/d;s/dn: DC=(.*)CN=MicrosoftDNS.*$/\1/;s/^([^,]+),DC=([^,]+),$/\1.\2/p'
