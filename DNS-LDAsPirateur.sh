306c5e2d14071f617da327c5e251ed165fcd8527
Rémi GASCOU (Podalirius) <podalirius@protonmail.com>
2023-05-11 17:47:42 +0200

    Set default SASL quiet mode

--- DNS-LDAsPirateur.sh
+++ DNS-LDAsPirateur.sh
@@ -27,4 +27,4 @@ while [[ -n "$1" ]] ; do
 	      esac
 done
 
-ldapsearch -LLL -H ldap://${ldap}:389 -b "CN=MicrosoftDNS,DC=DomainDnsZones,${domain}"  -w "${password}" -U "${user}" '(objectClass=dnsNode)' -E pr=${results_per_page}/noprompt | grep -A 1 '^dn:' | sed -nE '/--/d;s/dn: DC=(.*)CN=MicrosoftDNS.*$/\1/;s/^([^,]+),DC=([^,]+),$/\1.\2/p'
+ldapsearch -Q -LLL -H ldap://${ldap}:389 -b "CN=MicrosoftDNS,DC=DomainDnsZones,${domain}"  -w "${password}" -U "${user}" '(objectClass=dnsNode)' -E pr=${results_per_page}/noprompt | grep -A 1 '^dn:' | sed -nE '/--/d;s/dn: DC=(.*)CN=MicrosoftDNS.*$/\1/;s/^([^,]+),DC=([^,]+),$/\1.\2/p'
